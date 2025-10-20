fc583c9c35a28b9c89e717dcc22514c759dc2a9e
DPS2004 <dpsophinos@gmail.com>
2025-10-20 11:04:13 -0400

    start grid

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public enum spaceType
+    {
+        Empty,
+        Rough,
+        Wall,
+        Goal
+    }
+    public int width = 10;
+    public int height = 10;
+    public float cellWidth = 1f;
+    public float cellHeight = 1f;
+
+    public GameObject gridObject;
+
+    GridObject[,] grid;
+
+    void Start()
+    {
+        MakeGrid();
+
+    }
+
+
+    void MakeGrid()
+    {
+        grid = new GridObject[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = Instantiate(gridObject, gameObject.transform).GetComponent<GridObject>();
+                grid[x, y].x = x;
+                grid[x, y].y = y;
+                grid[x, y].spaceType = spaceType.Empty;
+                grid[x, y].GridInit();
+            }
+        }
+
+    }
+
+    public Vector3 GetWorldPosition(float x, float y)
+    {
+        return new Vector3(x * cellWidth - ((float)width /2), 0, y * cellHeight - ((float)height / 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int x;
+    public int y;
+    public GridManager.spaceType spaceType;
+    GridManager gridManager;
+    public Transform cubePivot;
+    public Renderer cubeMesh;
+
+    public Material emptyMaterial;
+    public Material goalMaterial;
+    public Material roughMaterial;
+    public Material wallMaterial;
+
+    Dictionary<GridManager.spaceType, Material> spaceTypeMaterials;
+
+    void Awake()
+    {
+        spaceTypeMaterials = new Dictionary<GridManager.spaceType, Material>()
+        {
+            {GridManager.spaceType.Empty, emptyMaterial },
+            {GridManager.spaceType.Goal, goalMaterial },
+            {GridManager.spaceType.Rough, roughMaterial },
+            {GridManager.spaceType.Wall, wallMaterial },
+        };
+    }
+
+    public void GridInit()
+    {
+        gridManager = GetComponentInParent<GridManager>();
+        transform.position = gridManager.GetWorldPosition(x, y);
+        UpdateSpaceTypeDisplay();
+    }
+
+    public void UpdateSpaceTypeDisplay()
+    {
+        if(spaceType != GridManager.spaceType.Wall)
+        {
+            cubePivot.localScale = new Vector3(0.9f, 0.1f, 0.9f);
+        }
+        else
+        {
+            cubePivot.localScale = new Vector3(0.9f, 1f, 0.9f);
+
+        }
+
+        cubeMesh.material = spaceTypeMaterials[spaceType];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
